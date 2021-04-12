363d966682e4f76a69b0b8d6d56a71d1ca06a080
Kacper Sokolowski <Kacperbxl@gmail.com>
2021-04-12 20:06:42 +0200

    basics

--- intro.dart
+++ intro.dart
@@ -1,3 +1,29 @@
 void main() {
     print("Hello world!");
+
+    var name = "Dart Introduction";
+    
+    var year = 2021;
+    
+    var planets = ["Earth", "Jupiter", "Saturn", "Mars", "Uranus"];
+
+    for(var planet in planets) {
+      print(planet);
+    }
+
+    while (year < 2025) {
+      year+=1;
+      print(year);
+    }
+
+    int fibonacci(int n) {
+      if(n == 0 || n == 1) return n;
+      return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    var result = fibonacci(20);
+    print(result);
+
+    planets.where((name) => name.contains('turn')).forEach(print);
+    
 }
